01497a55f97f759562d101d4123dccf636891a8f
MasatoshiTsunemi <yubi_deppou@yahoo.co.jp>
2019-07-28 22:45:28 +0900

    演習10完成手前

--- JavaScript演習問題/演習10/script.js
+++ JavaScript演習問題/演習10/script.js
@@ -49,75 +49,89 @@ $(document).ready(function(){
     $('#modal_contents').fadeIn('slow');
     $('#modal_overlay').fadeIn('slow');
 
-    // 登録ボタンがクリックされたら新講座名を表示
-    $('#entry').click(function(){
+    // 新規登録ボタンがクリックされたら、marker_newクラスを追加
+    $('.seminar').addClass('marker_new')
+  });
 
-      // textの文字数が１文字以上、20文字以下が入力されていたら新講座名を表示
-      if(($('#text').val() !== "") && ($('#text').val().length <= 20)){
-        return false;
-        var add_li = $("<li class='seminar' id=" + course_id + "></li>");
-        var add_h2 = $("<h2> " + $('#text').val() + " </h2>");
-        var add_p_check = $("<p class='check'>空き席状況を確認</p>");
-        var add_p_edit = $("<p class='edit'>編集</p>");
-        var add_p_delete = $("<p class='delete'>削除</p>");
-        $(add_li).append(add_h2, add_p_check, add_p_edit, add_p_delete);
-        $('.list').append(add_li);
+  // 登録ボタンがクリックされたら新講座名を表示
+  $('#entry').click(function(){
 
-        // 講座オブジェクトに講座の新規登録情報の追加を反映
-        course_object.push(
-          {"id":String(course_id),
-          "name":$('#text').val(),
-          "crowded":"no"});
+    // textの文字数が１文字以上、20文字以下が入力されていたら新講座名を表示
+    if(($('#text').val() !== "") && 
+       ($('#text').val().length <= 20) && 
+       ($('.seminar').hasClass('marker_new'))){
+    var add_li = $("<li class='seminar' id=" + course_id + "></li>");
+    var add_h2 = $("<h2> " + $('#text').val() + " </h2>");
+    var add_p_check = $("<p class='check'>空き席状況を確認</p>");
+    var add_p_edit = $("<p class='edit'>編集</p>");
+    var add_p_delete = $("<p class='delete'>削除</p>");
+    $(add_li).append(add_h2, add_p_check, add_p_edit, add_p_delete);
+    $('.list').append(add_li);
 
-        // 講座オブジェクトidを更新
-        course_id += 1;
+    // 講座オブジェクトに講座の新規登録情報の追加を反映
+    course_object.push(
+      {"id":String(course_id),
+       "name":$('#text').val(),
+       "crowded":"no"});
 
-        // txetに文字が入力されていたら、モーダルを閉じる
-        $('#modal_contents, #modal_overlay').fadeOut('slow');
-        $('#text').val("");
-      } else {
-        window.alert("文字数は1文字以上20文字以下で登録してください")
-      }
-    });
+      // 講座オブジェクトidを更新
+      course_id += 1;
+
+      // txetに文字が入力されていたら、モーダルを閉じる
+      $('#modal_contents, #modal_overlay').fadeOut('slow');
+      $('#text').val("");
+
+      // seminarクラスからmarker_newクラスを削除
+      $('.seminar').removeClass('marker_new');
+    } else {
+      window.alert("文字数は1文字以上20文字以下で登録してください")
+    }
   });
 
   // 閉じるボタンかモーダルオーバーレイがクリックされたらモーダルとモーダルオーバーレイを閉じる
   $('#close, #modal_overlay').click(function(){
     $('#modal_contents, #modal_overlay').fadeOut('slow')
 
-    // モーダルが閉じたらテキスト内の文字を消去
+    // テキスト内の文字を消去
     $('#text').val("");
+
+    // marker_newとmarker_editクラスを削除
+    $('.seminar').removeClass('marker_new');
+    $('.seminar').removeClass('marker_edit');
   });
 
   // 編集ボタンがクリックされたら、モーダルを開く
   $('.list').on('click', '.edit', function(){
     $('#modal_contents').fadeIn('slow');
     $('#modal_overlay').fadeIn('slow');
-    $(this).parent('li').addClass('marker')
-    var course = $(this).siblings('h2');
 
-
+    // 編集ボタンがクリックされたら、marker_editクラスを追加
+    $(this).parent('li').addClass('marker_edit')
 
     // 現在の講座名をtextに表示
-    $('#text').val($(course).text())
+    $('#text').val($($(this).siblings('h2')).text())
   });
 
-  // 登録ボタンがクリックされたら講座名を編集
+  // 登録ボタンのクリックされたら講座名を編集
   $('#entry').click(function(){
 
-    // txetに文字が入力されていたら講座名を編集
-    if($('#text').val() !== ""){
-      console.log(course.text());
-      $(course).text($('#text').val())
+    // 条件が成立していれば、講座名を編集
+    if(($('#text').val() !== "") && ($('.seminar').hasClass('marker_edit'))){
+      $('.marker_edit').children('h2').text(($('#text').val()));
 
       // 講座オブジェクトに編集を反映
+      course_object[$('.marker_edit').attr('id')].name = $('#text').val();
 
 
-      // txetに文字が入力されていたら、モーダルを閉じる
+      // 講座名が編集されたら、モーダルを閉じる
       $('#modal_contents, #modal_overlay').fadeOut('slow');
       $('#text').val("");
     }
+
+    // seminarクラスからmarker_editクラスを削除
+    $('.seminar').removeClass('marker_edit');
   });
+
 
   // 削除ボタンがクリックされたら、その講座名を消去
   $('.list').on('click', '.delete', function(){
